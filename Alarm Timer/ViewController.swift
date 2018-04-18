5c819bd623529a8d1fc460cbbe4fb17fdfe15480
DIT <d7703_21@d7703-21ui-imac.local>
2018-04-18 12:42:39 +0900

    color

--- Alarm Timer/ViewController.swift
+++ Alarm Timer/ViewController.swift
@@ -10,16 +10,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myDatePicker: UIDatePicker!
+    @IBOutlet weak var alramTime: UILabel!
+    
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm:ss"
+        
+        // alramTime레이블에 표시
+        alramTime.text = formatter.string(from: myDatePicker.date)
+        
+        // 1초마다 변경하게해주는 코드
+        myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in
+            self.updateTime()
+        })
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func updateTime() {
+        
+        // 시간을 만드는 코드
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm:ss"
+        
+        // currentTimeLabel 레이블에 표시
+        currentTimeLabel.text = formatter.string(from: date)
+        
+        // 빨갱이 만들어주는거
+        if currentTimeLabel.text == alramTime.text {
+            view.backgroundColor = UIColor.red
+        }
     }
-
-
+    
+  
+    @IBAction func DatePicker(_ sender: Any) {
+            // 데이터피커에 date속성을 변수에저장
+            //let myAlramTime = myDatePicker.date
+            //레이블에 텍스트를 보여주는 코드
+            //alramTime.text = String(describing: myAlramTime)
+        
+        
+        
+        //데이터피커의 정보를 받아 실시간으로 밑에 숫자를 뜨게하는거
+            let formatter = DateFormatter()
+            formatter.dateFormat = "hh:mm:ss"
+            alramTime.text = formatter.string(from: myDatePicker.date)
+        
+        
+    }
+    
+    
+    @IBAction func Reset(_ sender: Any) {
+        //리셋버튼
+        view.backgroundColor = UIColor.white
+    }
+    
 }
 
